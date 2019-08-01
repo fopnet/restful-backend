d101f1ba0cadcee2af9223dafa7539d63f83f1de
Felipe Pina <fop.net@gmail.com>
2019-08-01 12:18:00 -0300

    Comment plugin

---
+++ testes/backend-developer/solution/src/main/java/com/clickbus/place/PlaceApplication.java
@@ -0,0 +1,22 @@
+package com.clickbus.place;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@EnableJpaRepositories({"com.clickbus.place"})
+@EnableJpaAuditing
+@ComponentScan({"com.clickbus.place"})
+public class PlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceApplication.class, args);
+	}
+	
+	
+} 
