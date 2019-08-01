a6e8a7acac1a4999bac68caaa589a55cf9e92a7c
Felipe Pina <fop.net@gmail.com>
2019-08-01 04:42:05 -0300

    junit 5

--- testes/backend-developer/solution/src/test/java/com/clickbus/PlaceServiceTest.java
+++ testes/backend-developer/solution/src/test/java/com/clickbus/PlaceServiceTest.java
@@ -1,28 +1,34 @@
 package com.clickbus;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
+//import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertFalse;
+//import static org.junit.Assert.assertNotNull;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.TransactionSystemException;
 
+import com.clickbus.place.PlaceApplication;
 import com.clickbus.place.exception.PlaceException;
 import com.clickbus.place.model.Place;
 import com.clickbus.place.repository.PlaceRepository;
 import com.clickbus.place.service.PlaceServiceImpl;
 
-@RunWith(MockitoJUnitRunner.class)
+//@RunWith(MockitoJUnitRunner.class)
+@SpringBootTest(classes = PlaceApplication.class)
 public class PlaceServiceTest {
 
 	@InjectMocks
@@ -37,149 +43,142 @@ public class PlaceServiceTest {
 
 		Mockito.when(placeRepository.findById(1L)).thenReturn(Optional.of(new Place(1l)));
 
-		assertEquals("Deveria retornar o place 1", placeRepository.findById(1l).get(), expected);
+		assertEquals(placeRepository.findById(1l).get(), expected, "Must return place 1");
 	}
 
-	@Test(expected = PlaceException.class)
+	@Test
 	public void testGetIdNotExists() {
 
 		Mockito.when(placeRepository.findById(1L)).thenReturn(Optional.empty());
 
-		this.placeService.getPlaceById(1l);
-
+		assertThrows(PlaceException.class, () -> this.placeService.getPlaceById(1l));
 	}
-	
-	@Test(expected = PlaceException.class)
+
+	@Test
 	public void testGetNameByNameNotExists() {
-		
+
 		@SuppressWarnings("unchecked")
 		List<Place> emptyList = Collections.EMPTY_LIST;
-		
+
 		Mockito.when(placeRepository.findByName("pina")).thenReturn(emptyList);
-		
-		this.placeService.getPlacesByName("pina");
-		
+
+		assertThrows(PlaceException.class,() -> this.placeService.getPlacesByName("pina"));
+
 	}
-	
+
 	@Test()
 	public void testGetNameByNameExists() {
-		
+
 		Place p = new Place(1l);
 		p.setName("pina");
-		
+
 		List<Place> expected = Arrays.asList(p);
-		
+
 		Mockito.when(placeRepository.findByName("pina")).thenReturn(expected);
-		
+
 		List<Place> result = this.placeService.getPlacesByName("pina");
-		
-		assertFalse("Must not be null", result.isEmpty());
-		assertEquals("Must not be null", result.get(0), p);
-		assertEquals("Must not be null", result.get(0).getName(), p.getName());
-		
+
+		assertFalse(result.isEmpty(), "Must not be null");
+		assertEquals(result.get(0), p, "Must not be null");
+		assertEquals(result.get(0).getName(), p.getName(), "Must not be null");
+
 	}
-	
+
 	@Test()
 	public void testCreatePlace() {
-		
+
 		Place expected = new Place(1l);
 		expected.setName("pina");
 		expected.setSlug("slug");
 		expected.setState("RJ");
 		expected.setCity("city");
-		
-		
+
 		Mockito.when(placeRepository.save(expected)).thenReturn(expected);
-		
+
 		Place result = this.placeService.createPlace(expected);
-		
-		assertNotNull("Must not be null", result);
-		assertEquals("Must be the extected place", result, expected);
-		
+
+		assertNotNull(result, "Must not be null");
+		assertEquals(result, expected, "Must be the extected place");
+
 	}
-	
-	@Test(expected=TransactionSystemException.class)
+
+	@Test
 	public void testCreatePlaceNullValues() {
-		
+
 		Place expected = new Place(1l);
 		expected.setName(null);
 		expected.setSlug("slug");
 		expected.setState("RJ");
 		expected.setCity("city");
-		
-		
+
 		Mockito.when(placeRepository.save(expected)).thenThrow(TransactionSystemException.class);
-		
-		this.placeService.createPlace(expected);
-		
-	}
-	
-	@Test
-	public void testUpdatePlace() {
-		
-		Place expected = new Place(1l);
-		expected.setName("pina");
-		expected.setSlug("slug");
-		expected.setState("RJ");
-		expected.setCity("city");
-		
-		
-		Mockito.when(placeRepository.findById(1l)).thenReturn(Optional.of(expected));
-		
-		assertEquals("Plane name must by pina", expected.getName(), "pina");
-		
-		expected.setName("pina2");
-		
-		Mockito.when(placeRepository.save(expected)).thenReturn(expected);
-		
-		Place result = this.placeService.updatePlace(expected, 1l);
-		
-		assertNotNull("Must not be null", result);
-		assertEquals("Must be the expected place", result, expected);
-		assertEquals("Must be the expected place", result.getName(), "pina2");
-		
-	}
-	
-	
-	@Test(expected=PlaceException.class)
-	public void testUpdatePlaceNotExists() {
-		
-		Place expected = new Place(1l);
-		expected.setName("pina");
-		expected.setSlug("slug");
-		expected.setState("RJ");
-		expected.setCity("city");
-		
-		
-		Mockito.when(placeRepository.findById(1l)).thenReturn(Optional.empty());
-		
-		this.placeService.updatePlace(expected, 1l);
-		
-	}
-	
-	@Test
-	public void testDeletePlace() {
-		
-		Place expected = new Place(1l);
-		expected.setName("pina");
-		expected.setSlug("slug");
-		expected.setState("RJ");
-		expected.setCity("city");
-		
-		
-		Mockito.when(placeRepository.findById(1l)).thenReturn(Optional.of(expected));
-		
-		placeService.deleteById(1l);
-		
+
+		assertThrows(TransactionSystemException.class, () -> this.placeService.createPlace(expected));
+
 	}
 
-	@Test(expected=PlaceException.class)
-	public void testDeletePlaceNotExists() {
-		
+	@Test
+	public void testUpdatePlace() {
+
+		Place expected = new Place(1l);
+		expected.setName("pina");
+		expected.setSlug("slug");
+		expected.setState("RJ");
+		expected.setCity("city");
+
+		Mockito.when(placeRepository.findById(1l)).thenReturn(Optional.of(expected));
+
+		assertEquals( expected.getName(), "pina", "Place name must by pina");
+
+		expected.setName("pina2");
+
+		Mockito.when(placeRepository.save(expected)).thenReturn(expected);
+
+		Place result = this.placeService.updatePlace(expected, 1l);
+
+		assertNotNull(result, "Must not be null");
+		assertEquals(result, expected, "Must be the expected place");
+		assertEquals(result.getName(), "pina2", "Must be the expected place");
+
+	}
+
+	@Test
+	public void testUpdatePlaceNotExists() {
+
+		Place expected = new Place(1l);
+		expected.setName("pina");
+		expected.setSlug("slug");
+		expected.setState("RJ");
+		expected.setCity("city");
+
 		Mockito.when(placeRepository.findById(1l)).thenReturn(Optional.empty());
-		
+
+		assertThrows(PlaceException.class, () -> this.placeService.updatePlace(expected, 1l));
+
+	}
+
+	@Test
+	public void testDeletePlace() {
+
+		Place expected = new Place(1l);
+		expected.setName("pina");
+		expected.setSlug("slug");
+		expected.setState("RJ");
+		expected.setCity("city");
+
+		Mockito.when(placeRepository.findById(1l)).thenReturn(Optional.of(expected));
+
 		placeService.deleteById(1l);
-		
+
+	}
+
+	@Test
+	public void testDeletePlaceNotExists() {
+
+		Mockito.when(placeRepository.findById(1l)).thenReturn(Optional.empty());
+
+		assertThrows(PlaceException.class, () -> placeService.deleteById(1l));
+
 	}
 
 }
